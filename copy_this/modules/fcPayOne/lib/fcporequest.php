90f5692a6415aaf7a048d4d23832a8347a8a47c2
Andre Herrmann <andre.herrmann@fatchip.de>
2017-06-08 19:01:41 +0200

    OXID-130: Extracted check for correction to own method. Exchanged INSERT INTO to REPLACE INTO statement to not run into eternal checking if address will be corrected

--- copy_this/modules/fcPayOne/lib/fcporequest.php
+++ copy_this/modules/fcPayOne/lib/fcporequest.php
@@ -1730,7 +1730,8 @@ class fcpoRequest extends oxSuperCfg {
         foreach ($aAddressParameters as $sParamKey) {
             $sParamValue = $this->getParameter($sParamKey);
             if ($sParamValue) {
-                if ($aResponse !== false && array_key_exists($sParamKey, $aResponse) !== false && $aResponse[$sParamKey] != $sParamValue) {
+                $blCorrectAddressParam = $this->_fcpoCorrectAddressParam($sParamKey, $sParamValue, $aResponse);
+                if ($blCorrectAddressParam) {
                     //take the corrected value from the address-check
                     $sParamValue = $aResponse[$sParamKey];
                 }
@@ -1739,6 +1740,24 @@ class fcpoRequest extends oxSuperCfg {
         }
         $sHash = md5($sAddress);
         return $sHash;
+    }
+
+    /**
+     * Check response against current addressdata
+     *
+     * @param $sParamKey
+     * @param $sParamValue
+     * @param $aResponse
+     * @return bool
+     */
+    protected function _fcpoCorrectAddressParam($sParamKey, $sParamValue, $aResponse) {
+        $blCorrectAddressParam = (
+            $aResponse !== false &&
+            array_key_exists($sParamKey, $aResponse) !== false &&
+            $aResponse[$sParamKey] != $sParamValue
+        );
+
+        return $blCorrectAddressParam;
     }
 
     /**
@@ -1763,7 +1782,7 @@ class fcpoRequest extends oxSuperCfg {
      */
     protected function _saveCheckedAddress($aResponse) {
         $sCheckHash = $this->_getAddressHash($aResponse);
-        $sQuery = "INSERT INTO fcpocheckedaddresses ( fcpo_address_hash ) VALUES ( '{$sCheckHash}' )";
+        $sQuery = "REPLACE INTO fcpocheckedaddresses ( fcpo_address_hash ) VALUES ( '{$sCheckHash}' )";
         oxDb::getDb()->Execute($sQuery);
     }
 
@@ -1774,7 +1793,7 @@ class fcpoRequest extends oxSuperCfg {
      * @return array;
      */
     public function sendRequestConsumerscore($oUser) {
-        // Consumerscore nur f?r Deutschland zul?ssig
+        // Consumerscore only allowed in germany
         if ($this->getCountryIso2($oUser->oxuser__oxcountryid->value) == 'DE') {
             $oConfig = $this->getConfig();
             $this->addParameter('request', 'consumerscore');
